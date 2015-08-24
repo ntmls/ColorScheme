8ce9657b3e309f0eaf8d5db96fc8eed49fdd811a
ntmls <ntmls@live.com>
2015-08-24 11:43:42 -0500

    clean up

--- src/ColorScheme.js
+++ src/ColorScheme.js
@@ -66,6 +66,7 @@ function findNearestCluster(red, green, blue, clusters) {
 
 function resetClusters(clusters) {
     return _.map(clusters, function (c) { return resetCluster(c); });
+    return _.map(clusters, resetCluster);
 }
 
 function resetCluster(cluster) {
