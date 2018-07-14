7e2bb47635a1849c99cee370c2d44b0364bb370c
Nathan Miles <ntmls@live.com>
2018-07-14 18:14:26 -0500

    Add css.

--- src/ui/Steps.js
+++ src/ui/Steps.js
@@ -4,14 +4,14 @@ var Steps = (function() {
     function ChooseImageStep() {
         this.previous = function(context) { }; 
         this.next = function(context) { 
-            console.log("clusterColorStep");
+            context.view.setTitle(context.CLUSTER_COLORS_TITLE);
             context.currentStep = clusterColorStep;
         }; 
     }
     
     function ClusterColorStep() {
         this.previous = function(context) { 
-            console.log("chooseImageStep")
+            context.view.setTitle(context.PICK_IMAGE_TITLE);
             context.currentStep = chooseImageStep;
         }; 
         this.next = function(context) {}; 
@@ -24,7 +24,13 @@ var Steps = (function() {
     
     function Context() {
         var self = this;
+        this.PICK_IMAGE_TITLE = "Pick an Image";
+        this.CLUSTER_COLORS_TITLE = "Cluster Colors";
         this.currentStep = null;
+        this.setView = function(view) {
+            this.view = view;
+            view.setTitle(this.PICK_IMAGE_TITLE);
+        };
         this.previous = function(){ 
             self.currentStep.previous(self);
         }; 
